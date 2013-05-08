a67dc726343b85d1c4d6e5b61b4504c0925dfd96
Alexander Orlov <slimnbu@mail.ru>
2013-05-08 10:58:31 +0300

    remove make file

--- setup.py
+++ setup.py
@@ -6,9 +6,14 @@ from setuptools.command.install import install as DistutilsInstall
 
 class MakeInstall(DistutilsInstall):
     def run(self):
-        os.system('make')
+        cmds = [
+            'sudo apt-get install gcc make libmysqlclient-dev',
+            'sudo gcc -fpic -Wall -I/usr/include/mysql -I. -shared lib_mysqludf_sys.c -o /usr/lib/mysql/plugin/lib_mysqludf_sys.SONAME',
+            'mysql -u root -p -e "DROP FUNCTION IF EXISTS sys_exec"',
+            """mysql -u root -p -e "CREATE FUNCTION sys_exec RETURNS int SONAME 'lib_mysqludf_sys.so'" """]
+        for cmd in cmds:
+            os.system(cmd)
         DistutilsInstall.run(self)
-        os.system('rm -rf build')
 
 
 setup(
