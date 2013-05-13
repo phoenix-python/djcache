026204d3afaddea8cc67ca56e23579078b4342a5
Alexander Orlov <slimnbu@mail.ru>
2013-05-13 15:23:08 +0300

    password from os environ

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ from setuptools.command.install import install as DistutilsInstall
 
 class MakeInstall(DistutilsInstall):
     def run(self):
+        password = os.environ.get('MYSQL_PASSWORD')
         cmds = [
             'sudo apt-get install gcc make libmysqlclient-dev',
             'sudo gcc -fpic -Wall -I/usr/include/mysql -I. -shared lib_mysqludf_sys.c -o /usr/lib/mysql/plugin/lib_mysqludf_sys.SONAME',
-            'mysql -u root -p -e "DROP FUNCTION IF EXISTS sys_exec"',
-            """mysql -u root -p -e "CREATE FUNCTION sys_exec RETURNS int SONAME 'lib_mysqludf_sys.so'" """]
+            'mysql -u root %s -e "DROP FUNCTION IF EXISTS sys_exec"' % (
+                '-p' if mysql_password is None else '--password="%s"' % password),
+            """mysql -u root %s -e "CREATE FUNCTION sys_exec RETURNS int SONAME 'lib_mysqludf_sys.so'" """ % (
+                '-p' if mysql_password is None else '--password="%s"' % password)]
         for cmd in cmds:
             os.system(cmd)
         DistutilsInstall.run(self)
